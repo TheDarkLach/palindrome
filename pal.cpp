8107c5b0d357ddfe301f9e1a66056ab2a779cf04
TheDarkLach <11thunderstorm11@gmail.com>
2020-09-21 18:58:51 -0700

    i broke it

--- pal.cpp
+++ pal.cpp
@@ -6,21 +6,32 @@ using namespace std;
 
 int main()
 {
-  char word[20];
+  char word[80];
   int i, length;
   int pal = 0;
 
   cout << "Enter word: ";
   cin >> word;
 
+  //word.erase(remove(word.begin(), word.end(), ' '),word.end());
+
+  
   length = strlen(word);
 
+  /*for (i = 0; i < length; i++)
+  {
+    if (word[i] == NULL)
+      {
+	//word.erase(remove(word.begin(), word.end(), ' '),word.end());
+	
+      }
+      }*/
+  
   for (i = 0; i < length; i++)
   {
-    if (word[i] != word[length - i - 1])
+    if (word[i] != word[length - i - 1] && word[i] == ' ')
     {
       pal = 1;
-      break;
     }
   }
   if (pal)
@@ -31,4 +42,5 @@ int main()
   {
     cout << "Is a palindrome";
   }
+  return 0;
 }
