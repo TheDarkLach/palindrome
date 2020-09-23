688cb6ddf646fda752bf571df883d275e0bb77b6
TheDarkLach <11thunderstorm11@gmail.com>
2020-09-22 22:16:33 -0700

    no comments

--- pal.cpp
+++ pal.cpp
@@ -1,5 +1,3 @@
-//Plaindrome program by Faizan Karim
-
 #include <iostream>
 #include <cstring>
 
@@ -7,8 +5,6 @@ using namespace std;
 
 bool punccheck(char x, char white [])
 {
-  //function to check punctuation
-  
   bool punccheck = false;
   for (int j = 0; j < strlen(white); j++)
     {
@@ -19,50 +15,38 @@ bool punccheck(char x, char white [])
     }
   return punccheck;
 }
+
 int main()
 {
-  //char array
-  char input [80];
-  
-  //remove punctuation
-  char whitelist [] = " ./;:',<>()?!@#$%^&*{}[]|";
-  
-  cout << "Enter word: ";
-  
-  cin.getline(input,sizeof(input));
-  
+  char rawinput [80];
+  char noinput[80];
   bool pal = true;
-  int whitelength = strlen(whitelist);
-  int length = strlen(input);
-  
-
-  for (int i=0; i<strlen(input); i++)
+  bool punc = false;
+  int i = 0, len = 0;
+  char whitelist [] = " ./;:',<>()?!@#$%^&*{}[]|";
+  cout << "Enter word: ";
+  cin.getline(rawinput,sizeof(rawinput));
+  int length = strlen(rawinput);
+  for (int i = 0; i != length; i++){
+    if (!punccheck(rawinput[i], whitelist)){
+      noinput[len] = rawinput[i];
+      len++;
+    }
+  } 
+  for (int g=0; g < strlen(noinput); g++)
   {
-    //make everything lower case
-     input[i] = (tolower(input[i]));
+    noinput[g] = (tolower(noinput[g]));
   }
-  
-  for (int i = 0; i < length; i++)
-  {
-    if (input[i] != input[length-i-1])
-    {
-      //if word doesn't match after removing spaces and punctuation
-      if (!punccheck(input[i], whitelist) && !punccheck(input[length-i-1], whitelist))
-      {
-        pal = false;
-      }
+  for (int i = 0; i < len; i++){
+    if (noinput[i] != noinput[len-i-1]){
+      pal = false;
     }
   }
-  //print if or not pali
-  if (pal == true)
-  {
-    cout << "\nIs a palindrome\n\n";
-    cout << input;
+  if (pal == true){
+    cout << "Palindrome";
   }
-  else
-  {
-    cout << "\nNot a palindrome\n\n";
-    cout << input;
+  else {
+    cout << "Not a palindrome";
   }
 }
 
